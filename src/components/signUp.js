326707da8737f11e494f2cfc84da01363f89f228
Viraj Patidar <vrj381@gmail.com>
2021-11-12 19:00:19 +0530

    signUp page completed

--- src/components/signUp.js
+++ src/components/signUp.js
@@ -1,5 +1,7 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import Copyright from './copyright'
+import axios from 'axios';
 
 //IMAGE
 import notes from '../images/notes.svg';
@@ -17,19 +19,104 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import blue from '@mui/material/colors/blue';
+import Snackbar from '@mui/material/Snackbar';
+import Slide from '@mui/material/Slide';
 
 const theme = createTheme();
 
-export default function SignInSide() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        // eslint-disable-next-line no-console
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
-        });
-    };
+function TransitionLeft(props) {
+	return <Slide {...props} direction="left" />;
+}
+
+export default function SignUp() {
+
+    const history = useHistory();
+	const initialFormData = Object.freeze({
+		email: '',
+		name: '',
+		password: '',
+	});
+
+	const [formData, updateFormData] = useState(initialFormData);
+	const [emailerror, setEmailerror] = useState(false);
+	const [nameerror, setNameerror] = useState(false);
+	const [passerror, setPasserror] = useState(false);
+
+
+    //Snackbar
+	const [open, setOpen] = useState(false);
+	const [transition, setTransition] = useState(undefined);
+
+
+    const handleChange = (e) => {
+		setEmailerror(false)
+		setNameerror(false)
+		setPasserror(false)
+
+		updateFormData({
+			...formData,
+			// Trimming any whitespace
+			[e.target.name]: e.target.value.trim(),
+		});
+	};
+
+    const handleSubmit = (e) => {
+		e.preventDefault();
+
+		console.log(formData);
+
+		// Validation
+		const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		let submit = true
+
+		setEmailerror(false)
+		setNameerror(false)
+		setPasserror(false)
+
+		if (formData.email === "" || !re.test(formData.email)) {
+			setEmailerror(true)
+			submit = false
+			console.log(submit)
+			console.log(formData.email)
+		}
+		if (formData.name === "" || formData.name.length < 3 || formData.name.length > 19 || /\d/.test(formData.name)) {
+			setNameerror(true)
+			submit = false
+			console.log(submit)
+			console.log(formData.name)
+		}
+		if (formData.password === "" || formData.password.length < 6) {
+			setPasserror(true)
+			submit = false
+			console.log(submit)
+			console.log(formData.password)
+		}
+
+		
+		if (submit) {
+			axios.post(`http://localhost:8000/api/register`, {
+					"email": formData.email,
+					"name": formData.name,
+					"password": formData.password,
+				})
+				.then((res) => {
+					console.log(res);
+					console.log(res.data);
+                    history.push('/login');
+				})
+				.catch(err => { 
+					console.log(err)
+					if (err.response.status === 404) {
+						setTransition(() => TransitionLeft);
+						setOpen(true);
+					}
+				});
+		}
+	};
+
+    const handleClose = () => {
+		setOpen(false);
+	};
 
     return (
         <ThemeProvider theme={theme}>
@@ -80,34 +167,44 @@ export default function SignInSide() {
                             <Grid container spacing={2}>
                                 <Grid item xs={12}>
                                     <TextField
-                                        autoComplete="given-name"
-                                        name="Name"
-                                        required
-                                        fullWidth
-                                        id="Name"
-                                        label="Name"
-                                    />
+										variant="outlined"
+										required
+										fullWidth
+										id="Name"
+										label="Name"
+										name="name"
+										autoComplete="name"
+										onChange={handleChange}
+										error={nameerror}
+									/>
                                 </Grid>
                                 <Grid item xs={12}>
                                     <TextField
-                                        required
-                                        fullWidth
-                                        id="email"
-                                        label="Email Address"
-                                        name="email"
-                                        autoComplete="email"
-                                    />
+										variant="outlined"
+										required
+										fullWidth
+										id="email"
+										label="Email Address"
+										type="email"
+										name="email"
+										autoComplete="email"
+										onChange={handleChange}
+										error={emailerror}
+									/>
                                 </Grid>
                                 <Grid item xs={12}>
                                     <TextField
-                                        required
-                                        fullWidth
-                                        name="password"
-                                        label="Password"
-                                        type="password"
-                                        id="password"
-                                        autoComplete="new-password"
-                                    />
+										variant="outlined"
+										required
+										fullWidth
+										name="password"
+										label="Password"
+										type="password"
+										id="password"
+										autoComplete="current-password"
+										onChange={handleChange}
+										error={passerror}
+									/>
                                 </Grid>
                             </Grid>
                             <Button
@@ -115,6 +212,7 @@ export default function SignInSide() {
                                 fullWidth
                                 variant="contained"
                                 sx={{ mt: 3, mb: 2 }}
+                                onClick={handleSubmit}
                             >
                                 Sign Up
                             </Button>
@@ -128,6 +226,14 @@ export default function SignInSide() {
                         </Box>
                         <Copyright/>
                     </Box>
+                    <Snackbar
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+						open={open}
+						onClose={handleClose}
+						TransitionComponent={transition}
+						message="User Already Exists!! Please use another ID"
+						key={'bottom center'}
+					/>
                 </Grid>
             </Grid>
         </ThemeProvider>
