709e0cd54c1a663769950c1ae3ac965a887a510e
Viraj Patidar <vrj381@gmail.com>
2021-11-12 20:38:41 +0530

    login page completed

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import axios from 'axios';
 import Copyright from './copyright'
 
 //MUI
@@ -13,19 +15,100 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Snackbar from '@mui/material/Snackbar';
+import Slide from '@mui/material/Slide';
 
 
 const theme = createTheme();
 
+function TransitionLeft(props) {
+	return <Slide {...props} direction="left" />;
+}
+
 export default function Login() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        // eslint-disable-next-line no-console
-        console.log({
-            email: data.get('email'),
-            password: data.get('password'),
+
+    const history = useHistory();
+    const initialFormData = Object.freeze({
+        email: '',
+        password: '',
+    });
+
+    const [formData, updateFormData] = useState(initialFormData);
+    const [emailerror, setEmailerror] = useState(false);
+    const [passerror, setPasserror] = useState(false);
+    const [open, setOpen] = useState(false);
+    const [transition, setTransition] = useState(undefined);
+
+    const [message, setMessage] = useState("");
+
+    const handleChange = (e) => {
+
+        setEmailerror(false)
+        setPasserror(false)
+
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim(),
         });
+    };
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+
+        console.log(formData);
+
+        // Validation
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let submit = true
+
+        setEmailerror(false)
+        setPasserror(false)
+
+        if (formData.email === "" || !re.test(formData.email)) {
+            setEmailerror(true)
+            submit = false
+            console.log(submit)
+            console.log(formData.email)
+        }
+        if (formData.password === "" || formData.password.length < 6) {
+            setPasserror(true)
+            submit = false
+            console.log(submit)
+            console.log(formData.password)
+        }
+
+
+        if (submit) {
+            axios.post(`http://localhost:8000/api/login`, {
+                    "email": formData.email,
+                    "password": formData.password,
+                }, {withCredentials: true})
+                .then((res) => {
+                    console.log(res);
+                    console.log(res.data);
+
+                    history.push('/notes');
+
+                })
+                .catch(err => { 
+					console.log(err)
+					if (err.response.status === 400) {
+                        setMessage("Invalid Login Credentials! Please Try Again")
+						setTransition(() => TransitionLeft);
+						setOpen(true);
+					}
+					else if (err.response.status === 404) {
+                        setMessage("User does not exist!! Please SignUp first")
+						setTransition(() => TransitionLeft);
+						setOpen(true);
+					}
+				});
+        }
+    };
+
+    const handleClose = () => {
+        setOpen(false);
     };
 
     return (
@@ -48,6 +131,7 @@ export default function Login() {
                     </Typography>
                     <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
                         <TextField
+                            variant="outlined"
                             margin="normal"
                             required
                             fullWidth
@@ -56,8 +140,11 @@ export default function Login() {
                             name="email"
                             autoComplete="email"
                             autoFocus
+                            onChange={handleChange}
+                            error={emailerror}
                         />
                         <TextField
+                            variant="outlined"
                             margin="normal"
                             required
                             fullWidth
@@ -66,12 +153,15 @@ export default function Login() {
                             type="password"
                             id="password"
                             autoComplete="current-password"
+                            onChange={handleChange}
+                            error={passerror}
                         />
                         <Button
                             type="submit"
                             fullWidth
                             variant="contained"
                             sx={{ mt: 3, mb: 2 }}
+                            onClick={handleSubmit}
                         >
                             Sign In
                         </Button>
@@ -85,6 +175,14 @@ export default function Login() {
                     </Box>
                 </Box>
                 <Copyright/>
+                <Snackbar
+                    anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                    open={open}
+                    onClose={handleClose}
+                    TransitionComponent={transition}
+                    message={message}
+                    key={'bottom center'}
+                />
             </Container>
         </ThemeProvider>
     );
