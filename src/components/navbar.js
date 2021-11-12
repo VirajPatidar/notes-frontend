ece5d33a6f6c50d84e3db6386144d604626dfd42
Viraj Patidar <vrj381@gmail.com>
2021-11-12 17:41:11 +0530

    basic navbar completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,37 @@
-const Navbar = () => {
+import * as React from 'react';
+
+//MUI
+import LoginIcon from '@mui/icons-material/Login';
+import StickyNote2OutlinedIcon from '@mui/icons-material/StickyNote2Outlined';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+export default function Navbar() {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        href="/"
+                    >
+                        <StickyNote2OutlinedIcon fontSize="large"/>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        NOTE
+                    </Typography>
+                    <Button color="inherit" variant="outlined" startIcon={<LoginIcon />} href="/login">
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
- 
-export default Navbar;
